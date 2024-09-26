dd931fcb1c22da3a16dbba619adc5e66ccfa803a
Roman <asssfsa010@gmail.com>
2024-09-27 00:42:45 +0300

    Add project files.

---
+++ Практична робота №2. Матвійчук Роман/Ex1_option8.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практична_робота__2.Матвійчук_Роман
+{
+    internal class Ex1_option8
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            const double x = 0.11578,y = 4.675, z = 4.654;
+            double res = (y + Math.Cos(z))/(4*y+2*x*Math.Sqrt(x+2));
+            Console.WriteLine("При x={0,10:F3}, y={1,5:N2}, z={2,7:N3}"+
+                "значення вирази рівне {3,8:F4}", x,y,z,res);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Практична робота №2. Матвійчук Роман/Ex2_option8.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практична_робота__2.Матвійчук_Роман
+{
+    internal class Ex2_option8
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            const double k= 14, m = 0.42, a = 5;
+            double x, y, Z;
+            x = Math.Sqrt(Math.Pow((k+6.1),3));
+            y = Math.Pow(Math.Log(k),4)+(Math.Pow(m,-6)/(m+5.3));
+            Z = Math.Cos((x * x) / (0.14)) + Math.Log(a / 5);
+            Console.WriteLine("При k={0}, m={1} та a={2} обчислено x={3,8:N3}, y={4,7:F3}, F={5,8:F4}", k,m,a,x, y, Z);
+            Console.ReadKey();
+        }
+    }
+}
